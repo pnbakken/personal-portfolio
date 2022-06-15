83b5035559716a75a06fbbd00507c4253addbbbf
Pål N. Bakken <p.bakken@gmail.com>
2022-06-15 17:20:54 +0200

    fleshed out projects section. getting content from api

--- site/js/modules/display/displayProjects.js
+++ site/js/modules/display/displayProjects.js
@@ -1,7 +1,59 @@
-export default function displayProjects(projects) {
+import { baseURL } from "../settings/baseURL.js";
+import { fetchFromAPI } from "../tools/interfaceAPI.js";
+import addLoader from "./display-utils/addLoader.js";
 
+export default async function displayProjects() {
+    const target = document.querySelector(".projects-container");
+    addLoader(target);
+
+    const projects = await fetchFromAPI("api/projects");
+    if (projects) {
+        target.innerHTML = buildProjectList(projects.data);
+        addClickListener(target);
+    }
+
+    console.log(projects);
+
+    
+    console.log(target);
 }
 
-function projectBox(project) {
+function buildProjectList(projects) {
+    let html = "";
+
+    projects.forEach((project) => html += projectToHTML(project.attributes));
+    return html;
+
+    function projectToHTML(project) {
+        console.log(project);
+        return `<div class="container-item project-item">
+                   
+                    <div class="item-image project-image" style="background-image: url('${project.image_url}');"></div>
+                    <h3 class="project-name">${project.name}</h3>
+        
+                    <div class="project-details">
+                        <p>${project.description}</p>
+                        <div class="skill-list"></div>
+                    </div>                    
+        
+
+                </div>`;
+    }
+}
+
+function addClickListener(target) {
+    const projects = target.querySelectorAll(".project-item");
+    projects.forEach((project) => project.addEventListener("click", () => openProject(project)));
+}
+
+function openProject(project) {
+    console.log("clicked on " + project);
+    const details = project.querySelector(".project-details");
+    console.log(details);
+    details.style = "display: flex";
+    details.addEventListener("click", () => {
+        console.log("details closed");
+    })
     
 }
+
--- site/js/script.js
+++ site/js/script.js
@@ -1,11 +1,7 @@
 import { fetchFromAPI } from "./modules/tools/interfaceAPI.js";
 import {baseURL} from "./modules/settings/baseURL.js";
 import displaySkills from "./modules/display/displaySkills.js";
+import displayProjects from "./modules/display/displayProjects.js";
 
-async function getContent() {
-    const content = await fetchFromAPI(baseURL + "api/skills");
-    console.log(content);
-    
-}
-
+displayProjects();
 displaySkills();
