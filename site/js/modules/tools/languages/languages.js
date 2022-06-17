e65dd9f3a0391f0d2313d3d0ad1f044fb1b1596e
Pål N. Bakken <p.bakken@gmail.com>
2022-06-17 18:13:22 +0200

    small css adjustments

--- site/js/modules/tools/languages/languages.js
+++ site/js/modules/tools/languages/languages.js
@@ -2,30 +2,40 @@ export const languages = {
     eng : {
         navHome: "Home",
         navWork: "Projects", 
-        navSkills: "Skills",
+        navTechnologies: "Technologies",
         navContact: "Contact",
         navAbout: `About me`,
+
         pageHeading: `Responsive, <span class="blue-text">elegant,</span> easy-to-use websites`, 
         bannerText: `The text I will be using in the hero banner; looking very nice and displayed in the language the user has selected.`,
         bannerCTA: `Get in touch`,
+
         projectsHeading: `Projects`,
+
         contactHeading: `Contact me`,
         contactFormHeading: `Send me a message`,
         socialHeading: `Find me other places`,
+
+        techsHeading: `Technologies`,
     },
 
     nob : {
         navHome: "Hjem",
         navWork: "Prosjekter", 
-        navSkills: "Teknologi",
+        navTechnologies: "Teknologi",
         navContact: "Ta kontakt",
         navAbout: `Om meg`, 
+
         pageHeading: `Responsive, <span class="blue-text">elegante</span> nettsider som er enkle å bruke`, 
         bannerText: `Dette er teksten jeg skal ha i helte-banneret. Ser bra ut og er oversatt til det språket brukeren har valgt`,
         bannerCTA: "Ta kontakt",
+
         projectsHeading: `Prosjekter`,
+
         contactHeading: `Kontakt meg`,
         contactFormHeading: `Send meg en melding`,
         socialHeading: `Finn meg andre steder`,
+        
+        techsHeading: `Teknologi`,
     },
 };
