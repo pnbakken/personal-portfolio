a0b6e2625d5e5caa693b789eccdb4fcb8dae3c5d
Pål N. Bakken <p.bakken@gmail.com>
2022-06-17 16:32:24 +0200

    added more language strings

--- site/js/modules/tools/languages/languages.js
+++ site/js/modules/tools/languages/languages.js
@@ -4,9 +4,14 @@ export const languages = {
         navWork: "Projects", 
         navSkills: "Skills",
         navContact: "Contact",
+        navAbout: `About me`,
         pageHeading: `Responsive, <span class="blue-text">elegant,</span> easy-to-use websites`, 
         bannerText: `The text I will be using in the hero banner; looking very nice and displayed in the language the user has selected.`,
-        bannerCTA: `Get in touch`
+        bannerCTA: `Get in touch`,
+        projectsHeading: `Projects`,
+        contactHeading: `Contact me`,
+        contactFormHeading: `Send me a message`,
+        socialHeading: `Find me other places`,
     },
 
     nob : {
@@ -14,8 +19,13 @@ export const languages = {
         navWork: "Prosjekter", 
         navSkills: "Teknologi",
         navContact: "Ta kontakt",
+        navAbout: `Om meg`, 
         pageHeading: `Responsive, <span class="blue-text">elegante,</span> nettsider som er enkle å bruke`, 
         bannerText: `Dette er teksten jeg skal ha i helte-banneret. Ser bra ut og er oversatt til det språket brukeren har valgt`,
         bannerCTA: "Ta kontakt",
+        projectsHeading: `Prosjekter`,
+        contactHeading: `Kontakt meg`,
+        contactFormHeading: `Send meg en melding`,
+        socialHeading: `Finn meg andre steder`,
     },
 };
