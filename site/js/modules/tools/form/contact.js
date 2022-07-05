dbce306ebd99f913f9f360fecbf91fad81f0e912
Pål Nicholas Bakken <p.bakken@gmail.com>
2022-07-05 21:45:38 +0200

    added contact form validation script

--- site/js/modules/tools/form/contact.js
+++ site/js/modules/tools/form/contact.js
@@ -1,10 +1,31 @@
-import {$} from "../helpers/domSelector.js";
+import {$, $all} from "../helpers/domSelector.js";
 
 
 export default (function contactForm() {
-    $("#contact-form").addEventListener("submit", (e) => {
+    const form = $("#contact-form");
+    form.addEventListener("submit", (e) => {
         e.preventDefault();
-        console.log("Form submit");
+        if (validateForm(form)) {
+            packageForm(form);
+        }
     })
     
 })();
+
+function validateForm(form) {
+    console.log("Validating form...");
+    let valid = true;
+    $all("input, textarea", form).forEach( (input) => {
+        if(!input.value.trim()) {
+            console.log(input);
+            input.classList.toggle("input-error");
+            valid = false;
+            console.log(input + " invalid");
+        }
+    });
+
+}
+
+function packageForm(form) {
+    console.log("Packing form...");
+}
--- site/js/modules/tools/helpers/domSelector.js
+++ site/js/modules/tools/helpers/domSelector.js
@@ -1,7 +1,8 @@
-export function $(selector) {
-    return document.querySelector(selector);
+export function $(selector, scope=document) {
+    return scope.querySelector(selector);
 }
 
-export function $all(selector) {
-    return document.querySelectorAll(selector);
+export function $all(selector, scope=document) {
+    return scope.querySelectorAll(selector);
 }
+
